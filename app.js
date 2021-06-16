a7459532562f2922c54feffb40b90b60e53c0935
ParkerC1999 <parkman.christiansen@gmail.com>
2021-06-16 16:15:24 -0600

    generate webpage function added

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template')
 
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
